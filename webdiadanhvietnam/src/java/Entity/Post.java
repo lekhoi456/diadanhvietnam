34d0c50bca9f38b8b08d7129d371561262523ef0
Clone-Code <53707551+Clone-Code@users.noreply.github.com>
2019-11-11 23:48:37 +0700

    edited
    đã sửa model

--- webdiadanhvietnam/src/java/Entity/Post.java
+++ webdiadanhvietnam/src/java/Entity/Post.java
@@ -5,7 +5,9 @@ import java.sql.Date;
 public class Post {
     private long id;
     private String title;
+    private String description;
     private String thumbnail;
+    private String content;
     private long post_author;
     private String guid;
     private Date post_date;
@@ -13,16 +15,39 @@ public class Post {
     private long landscape_id;
     private long province_id;
 
-    public Post(long id, String title, String thumbnail, long post_author, String guid, Date post_date, byte status, long landscape_id, long province_id) {
+    public Post(long id, String title, String description, String thumbnail, String content, long post_author, String guid, Date post_date, byte status, long landscape_id, long province_id) {
         this.id = id;
         this.title = title;
+        this.description = description;
         this.thumbnail = thumbnail;
+        this.content = content;
         this.post_author = post_author;
         this.guid = guid;
         this.post_date = post_date;
         this.status = status;
         this.landscape_id = landscape_id;
         this.province_id = province_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" + "id=" + id + ", title=" + title + ", description=" + description + ", thumbnail=" + thumbnail + ", content=" + content + ", post_author=" + post_author + ", guid=" + guid + ", post_date=" + post_date + ", status=" + status + ", landscape_id=" + landscape_id + ", province_id=" + province_id + '}';
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+  
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public long getId() {
--- webdiadanhvietnam/src/java/Model/PostModel.java
+++ webdiadanhvietnam/src/java/Model/PostModel.java
@@ -19,126 +19,91 @@ import java.util.logging.Logger;
  */
 public class PostModel {
     public ArrayList<Post> postList = new ArrayList<>();
+    public static int numberInPaging = 10;
     private static Connection conn;
     private static Statement st;
-    private static PreparedStatement pst;
     private static ResultSet rs;
 
-    /**
-     * 
-     * @param title
-     * @param thumbnail
-     * @param post_author
-     * @param guid
-     * @param post_date
-     * @param status
-     * @param landscape_id
-     * @param province_id 
-     */
-    public void addPost(String title, String thumbnail, long post_author, String guid, Date post_date, byte status, long landscape_id, long province_id) {
-        try {
-            String insertquery = "INSERT INTO `post`(`title`, `thumbnail`, `post_author`, `guid`, `post_date`, `status`, `landscape_id`, `province_id`) "
-                                + "VALUES (?,?,?,?,?,?,?,?)";
-            conn = ConnectDB.getConnection();
-            pst = conn.prepareStatement(insertquery);
-            pst.setString(1, title);
-            pst.setString(2, thumbnail);
-            pst.setLong(3, post_author);
-            pst.setString(4, guid);
-            pst.setDate(5, post_date);
-            pst.setByte(6, status);
-            pst.setLong(7, landscape_id);
-            pst.setLong(8, province_id);
-            pst.executeUpdate();
-            
-            Post a = new Post(postList.size() + 1, title, thumbnail, post_author, guid, post_date, status, landscape_id, province_id);
-            postList.add(a);
-            loadPost();
-        } catch (SQLException ex) {
-            Logger.getLogger(PostModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+     public PostModel(Connection conn) {
+        this.conn = conn;
     }
 
-    /**
-     * 
-     */
-    public void loadPost() {
+    public ArrayList<Post> getPaging(int page, String search, String sortColumn) throws SQLException {
         try {
-            conn = ConnectDB.getConnection();
-            st = conn.createStatement();
-            String Selectquery = "SELECT * FROM `post`";
-            rs = st.executeQuery(Selectquery);
-           postList = new ArrayList<>();
-            if (rs.isBeforeFirst()) {
-                postList.clear();
-                while (rs.next()) {
-                    long id = rs.getInt("post_id");
-                    String title = rs.getString("tittle");
-                    String thumnail = rs.getString("thumnail");
-                    long post_author = rs.getInt("post_author");
-                    String guid = rs.getString("guid");
-                    Date post_date = rs.getDate("post_date");
-                    Byte status = rs.getByte("status");
-                    long landscape = rs.getInt("landscape_id");
-                    long province  = rs.getInt("province_id");
-                    Post a = new Post(id, title, thumnail, post_author, guid, post_date, status, landscape, province);
-                    postList.add(a);
+            String sqlStr = "";
+            sqlStr += "SELECT * FROM post";
+            if (search != "") {
+
+            }
+            if (sortColumn != "") {
+
+            }
+
+            int sumOfPost = getNumberOfPost(page, search, sortColumn);
+            int sumOfPage = (int) Math.ceil(sumOfPost / numberInPaging);
+            int index = (page - 1) * numberInPaging;
+
+            sqlStr += " LIMIT " + index + ", " + numberInPaging;
+            this.st = this.conn.createStatement();
+            this.rs = this.st.executeQuery(sqlStr);
+            postList = new ArrayList<Post>();
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String titile = rs.getString("title");
+                String description = rs.getString("description");
+                String content = rs.getString("content");
+                String guid = rs.getString("guid");
+                String thumbnail = rs.getString("thumbnail");
+                Date post_date = rs.getDate("post_date");
+                long post_author = rs.getInt("post_author");
+                long landscape_id = rs.getInt("landscape_id");
+                long province_id = rs.getInt("province_id");
+                byte status  = rs.getByte("status");
+                
+                postList.add(new Post(id, titile, description, thumbnail, content, post_author, guid, post_date, status, landscape_id, province_id));
+            }
+        } catch (SQLException se) {
+            throw se;
+        }
+        return this.postList;
+    }
+
+    public int getNumberOfPost(int page, String search, String sortColumn) throws SQLException {
+        String sqlStr = "";
+        sqlStr += "SELECT count(*) as numberOfPost FROM landscape";
+
+        this.st = this.conn.createStatement();
+        this.rs = this.st.executeQuery(sqlStr);
+        rs.next();
+        return rs.getInt("numberOfPost");
+    }
+
+    public String getPagingString(int currentPage, String search, String sortColumn) throws SQLException {
+        String strPaging = "<ul class='pagination'>";
+        try {
+            int sumOfPost = getNumberOfPost(currentPage, search, sortColumn);
+            int sumOfPage = (int) Math.ceil(sumOfPost / numberInPaging);
+            for (int inPage = 1; inPage <= sumOfPage; inPage++) {
+                if (inPage == currentPage) {
+                    strPaging += "<li class='active'><a href='?page=" + inPage + "'>" + inPage + "</a></li>";
+                } else {
+                    strPaging += "<li><a href='?page=" + inPage + "'>" + inPage + "</a></li>";
                 }
             }
+            strPaging += "</ul>";
         } catch (SQLException ex) {
-            Logger.getLogger(PostModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Entity.Landscape.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return strPaging;
     }
-    
-    /**
-     * 
-     * @param id
-     * @param name
-     * @param description
-     * @param guid
-     * @param thumbnail 
-     */
-    public void updatePost(long id, String name, String description, String guid, String thumbnail) {
-        try {
-            String Updatequery = "UPDATE `landscape` SET `name`=?,`description`=?,`thumbnail`=?,`guid`=? WHERE id = ?";
-            
-            pst = conn.prepareStatement(Updatequery);
-            pst.setString(1, name);
-            pst.setString(2, description);
-            pst.setString(3, guid);
-            pst.setLong(4, id);
-            pst.executeUpdate();
-            loadPost();
-        } catch (SQLException ex) {
-            Logger.getLogger(PostModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
-    
-    /***
-     * 
-     * @param id 
-     */
-    public void deletePost(long id) {
-        try {
-            String Deletequery = "DELETE FROM `post` WHERE id = " + id;
-            conn.prepareStatement(Deletequery);
-            pst.executeUpdate();
-            loadPost();
-        } catch (SQLException ex) {
-            Logger.getLogger(PostModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-     /**
-     * convert string to date
-     *
-     * @param date
-     * @return
-     */
-    public String date2String(java.util.Date date) {
-        SimpleDateFormat formater2 = new SimpleDateFormat("YYYY-MM-dd");
-        return formater2.format(date);
-    }
+//     /**
+//     * convert string to date
+//     *
+//     * @param date
+//     * @return
+//     */
+//    public String date2String(java.util.Date date) {
+//        SimpleDateFormat formater2 = new SimpleDateFormat("YYYY-MM-dd");
+//        return formater2.format(date);
+//    }
 }
